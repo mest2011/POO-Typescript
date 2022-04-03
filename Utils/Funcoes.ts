93b577bac6687364b8dd479b025cf759abe973af
mest2011 <mest2011mest@gmail.com>
2022-04-03 19:56:12 -0300

    feat: added license plate validation function

--- Utils/Funcoes.ts
+++ Utils/Funcoes.ts
@@ -3,3 +3,9 @@ import { TIPO_DE_SETA } from "../Models/Models";
 export function piscarSeta(tipo: TIPO_DE_SETA): void {
   console.log("Seta acionada: ", tipo);
 }
+
+export function validaPlaca(placa: string): boolean {
+  const regexPlaca = /^[aA-zZ]{3}(-)?[0-9]{4}$/;
+  if (regexPlaca.test(placa)) return true;
+  return false;
+}
